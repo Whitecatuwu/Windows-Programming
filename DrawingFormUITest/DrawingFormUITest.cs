1cc0b2a760ca5995e644003b02238f0950a04f5b
uwuWhitecat <aisbn3710@gmail.com>
2025-01-11 01:04:59 +0800

    remaining save load

--- DrawingFormUITest/DrawingFormUITest.cs
+++ DrawingFormUITest/DrawingFormUITest.cs
@@ -1,29 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
 /// <summary>
 /// Summary description for MainFormUITest
 /// </summary>
-namespace MainFormUITest
+namespace DrawingForm.Tests
 {
     [TestClass()]
     public class DrawingFormUITest
     {
+        string[] _toolBarBtns = new string[5] { "Start", "Terminator", "Process", "Line", "Select" };
+
         private Robot _robot;
-        private const string APP_NAME =
-       "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+        //private const string APP_NAME =
+        //"Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
         private const string CALCULATOR_TITLE = "HW7";
-        private const string EXPECTED_VALUE = "顯示是 444";
-        private const string RESULT_CONTROL_NAME = "CalculatorResults";
+        //private const string EXPECTED_VALUE = "顯示是 444";
+        //private const string RESULT_CONTROL_NAME = "CalculatorResults";
+
+
+
         /// <summary>
         /// Launches the Calculator
         /// </summary>
         [TestInitialize()]
         public void Initialize()
         {
-            var projectName = "Hw3";
+            //var projectName = "hw3";
             string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
-            var targetAppPath = Path.Combine(solutionPath, projectName,"DrawingForm", "bin", "Debug","hw2.exe");
+            var targetAppPath = Path.Combine(solutionPath, "DrawingForm", "bin", "Debug", "hw2.exe");
+            File.WriteAllText("abc.txt", targetAppPath.ToString());
             //Console.WriteLine(targetAppPath);
             _robot = new Robot(targetAppPath, CALCULATOR_TITLE);
         }
@@ -36,31 +46,279 @@ namespace MainFormUITest
             _robot.CleanUp();
         }
 
-        /// <summary>
-        /// Runs the script: 123 + 321 =
-        /// </summary>
-        private void RunScriptAdd()
+        [TestMethod]
+        public void TestBasisRedoUndo()
         {
-            _robot.ClickButton("Select");
-            /*_robot.ClickButton("一");
-            _robot.ClickButton("二");
-            _robot.ClickButton("三");
-            _robot.ClickButton("加");
-            _robot.ClickButton("三");
-            _robot.ClickButton("二");
-            _robot.ClickButton("一");
-            _robot.ClickButton("等於");
-            */
+            BasisRedoUndo();
         }
 
-        /// <summary>
-        /// Tests that the result of 123 + 321 should be 444
-        /// </summary>
-        [TestMethod]
-        public void TestAdd()
+        //[TestMethod]
+        public void TestShapeMoveRedoUndo()
         {
-            RunScriptAdd();
-            _robot.AssertText(RESULT_CONTROL_NAME, EXPECTED_VALUE);
+            ShapeMoveRedoUndo();
+        }
+
+        //[TestMethod]
+        public void TestTextMoveRedoUndo()
+        {
+            TextMoveRedoUndo();
+        }
+
+        //[TestMethod]
+        public void TestTextEditUndoRedoUndo()
+        {
+            TextEditRedoUndo();
+        }
+
+        //[TestMethod]
+        public void TestGridViewRedoUndo()
+        {
+            GridViewRedoUndo();
+        }
+
+        //[TestMethod]
+        public void TestMeta()
+        {
+            _robot.Sleep(0.2);
+            BasisRedoUndo();
+            _robot.UndoUntilDisable();
+
+            _robot.Sleep(0.2);
+            ShapeMoveRedoUndo();
+            _robot.UndoUntilDisable();
+
+            _robot.Sleep(0.2);
+            TextMoveRedoUndo();
+            _robot.UndoUntilDisable();
+
+            _robot.Sleep(0.2);
+            TextEditRedoUndo();
+            _robot.UndoUntilDisable();
+
+            _robot.Sleep(0.2);
+            GridViewRedoUndo();
+            //_robot.UndoUntilDisable();
+
+        }
+
+        private void AssertToolBar(string name)
+        {
+            foreach (var btn in _toolBarBtns)
+            {
+                if (btn == name)
+                    _robot.AssertChecked(btn, true);
+                else
+                    _robot.AssertChecked(btn, false);
+            }
+        }
+        private void DrawStart(int index)
+        {
+            _robot.ClickButton("Start");
+            AssertToolBar("Start");
+
+            _robot.MouseMove(200, 100);
+            _robot.MouseDown();
+            _robot.MouseMove(200, 200);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", index, new string[] { "Start", "220", "139", "200", "200" });
+        }
+        private void DrawTerminator(int index)
+        {
+            _robot.ClickButton("Terminator");
+            AssertToolBar("Terminator");
+
+            _robot.MouseMove(200, 200);
+            _robot.MouseDown();
+            _robot.MouseMove(410, 210);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", index, new string[] { "Terminator", "245", "239", "210", "410" });
+        }
+        private void DrawProcess(int index)
+        {
+            _robot.ClickButton("Process");
+            AssertToolBar("Process");
+
+            _robot.MouseMove(200, 300);
+            _robot.MouseDown();
+            _robot.MouseMove(230, 230);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", index, new string[] { "Process", "269", "339", "230", "230" });
+        }
+        private void DrawDecision(int index)
+        {
+            _robot.ClickButton("Decision");
+            AssertToolBar("Decision");
+
+            _robot.MouseMove(400, 300);
+            _robot.MouseDown();
+            _robot.MouseMove(240, 240);
+            _robot.MouseUp();
+            //
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", index, new string[] { "Decision", "492", "339", "240", "240" });
+        }
+        private void DrawLine()
+        {
+            _robot.ClickButton("Line");
+            AssertToolBar("Line");
+
+            _robot.MouseMove(202, 101);
+            _robot.MouseDown();
+            _robot.MouseMove(1, 1);
+            _robot.MouseMove(130, 100);
+            _robot.MouseUp();
+        }
+
+        private void BasisRedoUndo()
+        {
+            DrawStart(0);
+            DrawTerminator(1);
+            DrawLine();
+            DrawProcess(2);
+            DrawDecision(3);
+
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 3);
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 4);
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 3, new string[] { "Decision", "492", "339", "240", "240" });
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 3);
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 2);
+            _robot.ClickButton("Undo"); //undo line
+            _robot.AssertDataGridViewRowCount("shapeGridView", 2);
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 1);
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 2);
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 1, new string[] { "Terminator", "245", "239", "210", "410" });
+            _robot.ClickButton("Redo"); //redo line
+            _robot.AssertDataGridViewRowCount("shapeGridView", 2);
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 3);
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 2, new string[] { "Process", "269", "339", "230", "230" });
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowCount("shapeGridView", 4);
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 3, new string[] { "Decision", "492", "339", "240", "240" });
+
+        }
+        private void ShapeMoveRedoUndo()
+        {
+            DrawStart(0);
+            DrawTerminator(1);
+            DrawLine();
+            _robot.MoveToElement("Start");
+
+            _robot.MouseMove(400, 300);
+            _robot.MouseMove(-150, -150);
+
+            _robot.MouseDown();
+            _robot.MouseMove(0, 50);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "220", "189", "200", "200" });
+
+            _robot.MouseDown();
+            _robot.MouseMove(50, 0);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "270", "189", "200", "200" });
+
+            _robot.MouseDown();
+            _robot.MouseMove(0, -70);
+            _robot.MouseUp();
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "270", "119", "200", "200" });
+
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "270", "189", "200", "200" });
+
+            _robot.ClickButton("Undo");
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "220", "189", "200", "200" });
+
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "270", "189", "200", "200" });
+
+            _robot.ClickButton("Redo");
+            _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, new string[] { "Start", "270", "119", "200", "200" });
+        }
+
+        private void TextMoveRedoUndo()
+        {
+            DrawStart(0);
+            _robot.MouseMove(-100, -110);
+
+            _robot.MouseDown();
+            _robot.MouseMove(0, 1);
+            _robot.MouseMove(0, 49);
+            _robot.MouseUp();
+            _robot.MouseDoubleClick();
+            _robot.AssertTextEditPopUp();
+
+            _robot.ClickButton("Undo");
+            _robot.MoveToElement("Start");
+            _robot.MouseMove(200, 100);
+            _robot.MouseMove(100, 90);
+            _robot.MouseDoubleClick();
+            _robot.AssertTextEditPopUp();
+
+            _robot.ClickButton("Redo");
+            _robot.MoveToElement("Start");
+            _robot.MouseMove(200, 100);
+            _robot.MouseMove(100, 90 + 50);
+            _robot.MouseDoubleClick();
+            _robot.AssertTextEditPopUp();
+        }
+
+        private void TextEditRedoUndo()
+        {
+            DrawStart(0);
+            string preText = _robot.GetTextFormGridView("shapeGridView", 0);
+            _robot.MouseMove(-100, -110);
+            _robot.MouseDoubleClick();
+            _robot.InputText("qwertyuiop");
+            _robot.AssertTextFromGridView("shapeGridView", 0, "qwertyuiop");
+            _robot.ClickButton("Undo");
+            _robot.AssertTextFromGridView("shapeGridView", 0, preText);
+            _robot.ClickButton("Redo");
+            _robot.AssertTextFromGridView("shapeGridView", 0, "qwertyuiop");
+        }
+
+        private void GridViewRedoUndo()
+        {
+            string[][] shapeDataList = new string[4][]
+            {
+                new string[] { "Start", "220", "139", "200", "200" },
+                new string[] { "Terminator", "245", "239", "210", "410" },
+                new string[] { "Process", "269", "339", "230", "230" },
+                new string[] { "Decision", "492", "339", "240", "240" }
+            };
+            int i = 0;
+            foreach (var shapeData in shapeDataList)
+            {
+                _robot.SelectShape("selectedShapeType", shapeData[0]);
+                _robot.InputShapeDatas(shapeData);
+                _robot.ClickButton("新增");
+                _robot.AssertDataGridViewRowDataBy("shapeGridView", i, shapeData);
+                i++;
+            }
+            for(int j = 0; j < 4; j++)
+            {
+                _robot.ClickButton("Undo");
+                _robot.AssertDataGridViewRowCount("shapeGridView", 3-j);
+            }
+            for (int j = 0; j < 4; j++)
+            {
+                _robot.ClickButton("Redo");
+                _robot.AssertDataGridViewRowDataBy("shapeGridView", j, shapeDataList[j]);
+            }
+            for(int j = 0; j < 4; j++)
+            {
+                _robot.ClickDataGridViewCellBy("shapeGridView", 0, "刪除");
+                _robot.AssertDataGridViewRowCount("shapeGridView", 3 - j);
+            }
+            for (int j = 3; j >= 0; j--)
+            {
+                _robot.ClickButton("Undo");
+                _robot.AssertDataGridViewRowDataBy("shapeGridView", 0, shapeDataList[j]);
+            }
         }
     }
 }
--- DrawingFormUITest/Robot.cs
+++ DrawingFormUITest/Robot.cs
@@ -9,8 +9,15 @@ using System.Collections.Generic;
 using OpenQA.Selenium;
 using System.Windows.Input;
 using System.Windows.Forms;
+using OpenQA.Selenium.Interactions;
+using System.Reflection;
+using System.Drawing.Printing;
 
-namespace MainFormUITest
+using System.IO;
+using static System.Windows.Forms.AxHost;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DrawingForm.Tests
 {
     public class Robot
     {
@@ -19,6 +26,16 @@ namespace MainFormUITest
         private string _root;
         private const string CONTROL_NOT_FOUND_EXCEPTION = "The specific control is not found!!";
         private const string WIN_APP_DRIVER_URI = "http://127.0.0.1:4723";
+
+        public int CoordX
+        {
+            get { return _driver.Manage().Window.Position.X; }
+        }
+
+        public int CoordY
+        {
+            get { return _driver.Manage().Window.Position.Y; }
+        }
 
         // constructor
         public Robot(string targetAppPath, string root)
@@ -108,6 +125,11 @@ namespace MainFormUITest
         // test
         public void CloseMessageBox()
         {
+            _driver.FindElementByName("取消").Click();
+        }
+
+        public void ConfirmEditText()
+        {
             _driver.FindElementByName("確定").Click();
         }
 
@@ -115,21 +137,61 @@ namespace MainFormUITest
         public void ClickDataGridViewCellBy(string name, int rowIndex, string columnName)
         {
             var dataGridView = _driver.FindElementByAccessibilityId(name);
-            _driver.FindElementByName($"{columnName} 資料列 {rowIndex}").Click();
+            dataGridView.FindElementByName($"{columnName} 資料列 {rowIndex}").Click();
+        }
+
+        public void AssertTextEditPopUp()
+        {
+            try
+            {
+                WindowsElement element = _driver.FindElementByName("編輯文字");
+                Sleep(1);
+                CloseMessageBox();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("TextEditForm do not pop-up.");
+                Assert.Fail();
+            }
         }
 
         // test
-        public void AssertEnable(string name, bool state)
+        public void AssertChecked(string name, bool state)
         {
             WindowsElement element = _driver.FindElementByName(name);
-            Assert.AreEqual(state, element.Enabled);
+            Assert.AreEqual(state ? "True" : "False", element.GetAttribute("HasKeyboardFocus"));
+        }
+
+        public void InputText(string text)
+        {
+            WindowsElement element = _driver.FindElementByAccessibilityId("TextEditForm");
+            var textBox = element.FindElementByAccessibilityId("textBox");
+            textBox.SendKeys(text);
+            ConfirmEditText();
+        }
+
+        public void UndoUntilDisable()
+        {
+            WindowsElement element = _driver.FindElementByName("Undo");
+            while (element.Enabled)
+            {
+                element.Click();
+            }
         }
 
         // test
-        public void AssertText(string name, string text)
+        public void AssertTextFromGridView(string name, int rowIndex, string text)
         {
             WindowsElement element = _driver.FindElementByAccessibilityId(name);
-            Assert.AreEqual(text, element.Text);
+            var rowDatas = element.FindElementByName($"資料列 {rowIndex}").FindElementsByXPath("//*");
+            Assert.AreEqual(text, rowDatas[5].Text.Replace("(null)", ""));
+        }
+
+        public string GetTextFormGridView(string name, int rowIndex)
+        {
+            WindowsElement element = _driver.FindElementByAccessibilityId(name);
+            var rowDatas = element.FindElementByName($"資料列 {rowIndex}").FindElementsByXPath("//*");
+            return rowDatas[5].Text.Replace("(null)", "");
         }
 
         // test
@@ -139,33 +201,96 @@ namespace MainFormUITest
             var rowDatas = dataGridView.FindElementByName($"資料列 {rowIndex}").FindElementsByXPath("//*");
 
             // FindElementsByXPath("//*") 會把 "row" node 也抓出來，因此 i 要從 1 開始以跳過 "row" node
-            for (int i = 1; i < rowDatas.Count; i++)
+            for (int i = 6; i < rowDatas.Count; i++)
             {
-                Assert.AreEqual(data[i - 1], rowDatas[i].Text.Replace("(null)", ""));
+                /* 機器人在畫圖時有時會誤差1 */
+                int diff = int.Parse(data[i - 5]) - int.Parse(rowDatas[i].Text.Replace("(null)", ""));
+                Assert.IsTrue(Math.Abs(diff) <= 1);
+                //Assert.AreEqual(data[i - 6], rowDatas[i].Text.Replace("(null)", ""));
+            }
+            Assert.AreEqual(data[0], rowDatas[4].Text.Replace("(null)", ""));
+        }
+
+        public void AssertDataGridViewRowCount(string name, int count)
+        {
+            var dataGridView = _driver.FindElementByAccessibilityId(name);
+            var rows = dataGridView.FindElementsByXPath(".//Custom"); // 假設每一行是 Custom 類型元素
+            Assert.AreEqual(count, rows.Count - 1);
+        }
+
+        public void OutputXml()
+        {
+            File.WriteAllText(@"output.xml", _driver.PageSource);
+            //"C:\Users\user\Desktop\material_list_2025-01-01_20.39.05.txt"
+        }
+
+        public void MouseDown()
+        {
+            var actions = new Actions(_driver);
+            actions.ClickAndHold().Perform();
+            Sleep(0.05d);
+        }
+
+        public void MouseUp()
+        {
+            var actions = new Actions(_driver);
+            actions.Release().Perform();
+            Sleep(0.05d);
+        }
+
+        public void MouseMove(int x, int y)
+        {
+            var actions = new Actions(_driver);
+            Sleep(0.1d);
+            actions.MoveByOffset(x, y).Perform();
+            Sleep(0.1d);
+        }
+
+        public void MouseDoubleClick()
+        {
+            var actions = new Actions(_driver);
+            actions.DoubleClick().Perform();
+
+            Sleep(0.05d);
+        }
+        public void MoveToElement(string name)
+        {
+            var actions = new Actions(_driver);
+            WindowsElement element = _driver.FindElementByName(name);
+            actions.MoveToElement(element).Perform();
+        }
+
+        public void SelectShape(string name, string type)
+        {
+            var element = _driver.FindElementByAccessibilityId(name);
+            new Actions(_driver).MoveToElement(element).Perform();
+            MouseMove(25, 0);
+            new Actions(_driver).Click().Perform();
+            element.FindElementByName(type).Click();
+        }
+
+        public void InputShapeDatas(string[] shapeDatas)
+        {
+            string[] textBoxs = new string[5]
+            {
+                "TextBoxText",
+                "TextBoxX",
+                "TextBoxY",
+                "TextBoxH",
+                "TextBoxW"
+            };
+            int i = 0;
+            foreach (var shapeData in shapeDatas) { 
+                var element = _driver.FindElementByAccessibilityId(textBoxs[i]);
+                element.Clear();
+                element.SendKeys(shapeData);              
+                i++;
             }
         }
 
-        // test
-        public void AssertDataGridViewRowCountBy(string name, int rowCount)
+        public void Test()
         {
-            var dataGridView = _driver.FindElementByAccessibilityId(name);
-            Point point = new Point(dataGridView.Location.X, dataGridView.Location.Y);
-            AutomationElement element = AutomationElement.FromPoint(point);
 
-            while (element != null && element.Current.LocalizedControlType.Contains("datagrid") == false)
-            {
-                element = TreeWalker.RawViewWalker.GetParent(element);
-            }
-
-            if (element != null)
-            {
-                GridPattern gridPattern = element.GetCurrentPattern(GridPattern.Pattern) as GridPattern;
-
-                if (gridPattern != null)
-                {
-                    Assert.AreEqual(rowCount, gridPattern.Current.RowCount);
-                }
-            }
         }
     }
 }
